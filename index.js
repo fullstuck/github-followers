e9bcda91b83fb8651f3d7e4d203344d133c8f6e2
M. Alif Raher <alifraher@gmail.com>
2018-08-01 10:51:03 +0700

    Optimize code

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 console.log("Level 3");
 
-let arr = [];
-
 const getDetail = () => {
-    fetch("https://api.github.com/users/alifraher/followers")
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            showDetail(data);
-        });
+    const url = "https://api.github.com/users/alifraher/followers"
+    fetch(url)
+        .then(response => response.json())
+        .then(showDetail);
 }
 
 const showDetail = (data) => {
-    const detail = data;
-    detail.map((details, index) => {
-        $("#output").append(template(detail, index))
-    });
+    data.forEach(item => {
+        $("#output").append(template(item));
+    })
 };
 
-const template = (follower, index) => {
+const template = (follower) => {
     return `<div class="card">
-                <img class="card-img-top" src="${follower[index].avatar_url}" alt="Card image cap">
+                <img class="card-img-top" src="${follower.avatar_url}" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">${follower[index].login}</p>
-                    <a href="${follower[index].url}" class="btn btn-primary btn-block">Go to profile</a>
+                    <h5 class="card-title">${follower.login}</h5>
+                    <a href="${follower.url}" class="btn btn-primary btn-block">Go to profile</a>
                 </div>
             </div>`
 }
